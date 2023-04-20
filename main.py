b45bd8f0d2d53b7edc1f8842f10085bb52435362
valerijvorobev <vorobjevvalera@gmail.com>
2023-04-20 09:42:52 +0300

    push

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import medium.validate_stack_sequences
+import others.snake_array
 
-medium.validate_stack_sequences.run()
+# medium.validate_stack_sequences.run()
+others.snake_array.run()
---
+++ others/snake_array.py
@@ -0,0 +1,35 @@
+# Вывести двумерный массив змейкой
+# На вход подается размер массива nxm
+# Массив заполняется цифрами по возрастанию с шагом 1
+
+def run():
+    print()
+    matrix = __zmeyka(5, 7)
+
+    for row in matrix:
+        for col in row:
+            print(f'{col} ', end='')
+        print()
+
+
+def __zmeyka(n, m) -> []:
+    matrix = []
+    number = 0
+
+    for i in range(0, n):
+        items = []
+        for j in range(0, m):
+            items.append(number)
+            number += 1
+        if i % 2 > 0:
+            reverse(items)
+        matrix.append(items)
+
+    return matrix
+
+
+def reverse(items: []):
+    n = len(items) - 1
+    a = n // 2
+    for i in range(0, a):
+        items[n - i], items[i] = items[i], items[n - i]
